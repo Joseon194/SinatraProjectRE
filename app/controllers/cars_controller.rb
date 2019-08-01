fff1d60d424f0096b40898f9775ac410515e8769
Dennis Chung <chungdennis6@gmail.com>
2019-08-01 17:10:44 +0000

    added lines to welcome.erb

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,89 @@
+class CarsController < ApplicationController
+
+    get '/cars/index' do
+        user = Helpers.current_user(session)
+        if user.nil?
+            redirect to '/signin'
+        else
+            @cars = Car.all
+            erb :'/cars/index'
+        end
+    end
+
+    get '/cars/new' do
+        user = Helpers.current_user(session)
+        if user.nil?
+            redirect to '/login'
+        else
+            erb :'/cars/create_car'
+        end
+    end
+
+    post '/cars/new' do
+        user = Helpers.current_user(session)
+        if user.nil?
+            redirect to '/signin'
+        elsif params[:car][:name].empty? || params[:car][:color].empty? || params[:car][:model_year].empty? || params[:car][:company].empty?
+            redirect to '/cars/new'
+        else
+            user.cars.new({name: params[:car][:name], color: params[:car][:color], model_year: params[:car][:model_year], company: params[:car][:company]})
+            user.save
+        end
+        redirect to '/cars/show'
+    end
+
+    get '/cars/show' do
+        user = Helpers.current_user(session)
+        if user.nil?
+            redirect to '/login'
+        else
+            @cars = user.cars.all
+            erb :'/cars/show_cars'
+        end
+    end
+
+    get '/cars/:id' do
+        if Helpers.is_signed_in?(session)
+            @cars = Car.find_by_id(params[:id])
+            erb :'/cars/edit_or_delete'
+        else
+            redirect to '/signin'
+        end
+    end
+
+    get '/cars/:id/edit' do
+        if !Helpers.is_signed_in?(session)
+            redirect to '/login'
+        end
+        @car = Car.find_by_id(params[:id])
+        if @car.user == Helpers.current_user(session)
+            erb :'/cars/edit_car'
+        else
+            redirect to '/signin'
+        end
+    end
+
+    patch '/cars/:id' do
+        if !Helpers.is_signed_in?(session)
+            redirect to '/signin'
+        end
+        @car = Car.find_by_id(params[:id])
+        if params[:car][:name].empty? || params[:car][:color].empty? || params[:car][:model_year].empty? || params[:car][:company].empty?
+            redirect to "/cars/#{@car.id}/edit"
+        end
+        @car.update(params[:car])
+        @car.save
+        redirect to '/cars/show'
+    end
+
+    delete '/cars/:id/delete' do
+        if !Helpers.is_signed_in?(session)
+            redirect to '/login'
+        end
+        @car = Car.find_by_id(params[:id])
+        if @car.user == Helpers.current_user(session)
+            @car.delete
+            redirect to '/cars/show'
+        end
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+
+      get '/signup' do
+          if Helpers.is_signed_in?(session)
+              redirect to '/home'
+          else
+              erb :'/users/signup'
+          end
+      end
+
+      post '/signup' do
+          if !(params.has_value?(""))
+              user = User.create(params)
+              session["user_id"] = user.id
+              redirect to '/home'
+          else
+              redirect to '/signup'
+          end
+      end
+
+      get '/signin' do
+          if Helpers.is_signed_in?(session)
+              redirect to '/home'
+          else
+              erb :'/users/signin'
+          end
+      end
+
+      post '/signin' do
+          user = User.find_by(username: params[:username])
+          if user && user.authenticate(params[:password])
+              session[:user_id] = user.email
+              redirect to '/home'
+          else
+              redirect to '/signin'
+          end
+      end
+
+      get '/home' do
+          if Helpers.is_signed_in?(session)
+              erb :'/users/home'
+          else
+              redirect to '/signin'
+          end
+      end
+
+      post '/signout' do
+          if Helpers.is_signed_in?(session)
+              session.clear
+              redirect to '/signin'
+          else
+              redirect to '/'
+          end
+      end
+    end
