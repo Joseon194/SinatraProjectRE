4f770c6b1f9a3d11690a0dda8d0c8b07efd4ca77
Dennis Chung <chungdennis6@gmail.com>
2019-08-01 17:49:55 +0000

    edited signin.erb

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/development.sqlite"
 )
 
 require './app/controllers/application_controller'
